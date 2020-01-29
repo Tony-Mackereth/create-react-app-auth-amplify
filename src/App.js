960f47f1ce248deccb12d91a525a04d3a32a7788
Tony Mackereth <tony.mackereth@myob.com>
2020-01-29 13:56:01 +1100

    Play around with getting a user.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
+const getUser = () => {
+  Auth.currentAuthenticatedUser()
+    .then(user => {
+      console.log(error);
+      return user;
+    })
+    .catch(err => console.log(err));
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <p>
+            Hi {getUser()}
+          </p>
+        </header>
       </div>
     );
   }
